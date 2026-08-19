8f3f967d4f416fe985721607a89be3a7d0dc9843
salvas999 <78573419+salvas999@users.noreply.github.com>
2026-08-19 18:42:15 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,6 @@ const PRODUCTS = [
   { id: 25, name: "Heineken 25cl", price: 12.48,  units: 24, img: "/images/heineken-25cl.png", category: "Cerveja" },
   { id: 26, name: "Frize Limão", price: 11.04, units: 24, img: "/images/frize-limao.png", category: "Águas" },
   { id: 27, name: "UCAL Leite Com Chocolate", price: 14,74, units: 24, img: "/images/ucal.png", category: "A Mais" },
-
 ];
 
 const PHONE = "351933499207";
