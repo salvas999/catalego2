6fba6023328d46a79371dd63ed0fa397e0ba3396
Salvador Carreira <78573419+salvas999@users.noreply.github.com>
2026-09-12 01:08:21 +0100

    Corrige imagens, animação e informação VOLTA

--- site.test.cjs
+++ site.test.cjs
@@ -12,7 +12,7 @@ function parseEuro(text) {
   const virtualConsole = new VirtualConsole();
   virtualConsole.on("jsdomError", error => browserErrors.push(error.message));
   const html = fs.readFileSync(path.join(__dirname, "index.html"), "utf8");
-  const script = fs.readFileSync(path.join(__dirname, "app.js"), "utf8");
+  const script = fs.readFileSync(path.join(__dirname, "app-v3.js"), "utf8");
   const dom = new JSDOM(html, {
     runScripts: "outside-only",
     pretendToBeVisual: true,
